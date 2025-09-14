b72383591391a73c970212a695e9eb53d6c03420
MichaelAngelo-Khel-ops <michael1118salonga@gmail.com>
2025-09-14 11:45:38 +0800

    LibraryManagertJavaScriptOOP

---
+++ src/script.js
@@ -0,0 +1,160 @@
+// 1. Create a Book class
+
+// Properties: title, author, year
+
+// Method: getSummary() → returns a string like "Title by Author, Year"
+
+// target id
+
+// class Book {
+//   #title;
+//   #author;
+//   #year;
+
+//   constructor(title, author, year) {
+//     this.#title = title;
+//     this.#author = author;
+//     this.#year = year;
+//   }
+
+//   getTitle() {
+//     return this.#title;
+//   }
+//   getAuthor() {
+//     return this.#author;
+//   }
+//   getYear() {
+//     return this.#year;
+//   }
+
+//   getSummary() {
+//     console.log(`${this.#title} by ${this.#author} ${this.#year}`);
+//   }
+// }
+
+// let button = document.querySelector("#exicution");
+
+// // create event for button to exicute;
+
+// const b1 = new Book("ang pangarap kong libreo", "mike", 2025);
+
+// b1.getSummary();
+
+// button.addEventListener("click", () => b1.getSummary());
+
+// Create a Book class (private fields: title, author, year)
+
+// Create a subclass Ebook that extends Book (add fileSize field)
+
+// Create multiple instances of Book and Ebook
+
+// Generate buttons dynamically for each book
+
+// When a button is clicked, display the summary of the selected book in a div
+
+class Book {
+  //private variables
+  #title;
+  #author;
+  #year;
+
+  constructor(title, author, year) {
+    this.#title = title;
+    this.#author = author;
+
+    if (typeof year != "number") {
+      throw new TypeError("Must be a number");
+    } else {
+      this.#year = year;
+    }
+  }
+
+  getTitle() {
+    return this.#title;
+  }
+  getAuthor() {
+    return this.#author;
+  }
+  getYear() {
+    return this.#year;
+  }
+
+  displayBook() {
+    return `${this.#title} by ${this.#author} : ${this.#year}`;
+  }
+}
+
+class Ebook extends Book {
+  #fileSize;
+
+  constructor(title, author, year, fileSize) {
+    super(title, author, year);
+
+    if (typeof fileSize == "string") {
+      this.#fileSize = parseInt(fileSize.replace("kb"));
+    } else {
+      this.#fileSize = fileSize;
+    }
+  }
+
+  displayBook(list) {
+    document.getElementById(
+      list
+    ).textContent = `${super.getTitle()} by ${super.getAuthor()} : ${super.getYear()} ${
+      this.#fileSize
+    }kb`;
+  }
+}
+
+// id selector
+function select(id) {
+  return document.querySelector(id);
+}
+
+// const ebook = new Ebook("Rascals", "Mike", 2025, 500);
+// add buttons
+const bookshelf = [];
+
+const books = {
+  author: ["mike", "diane", "pepe", "king", "jorme"],
+  title: ["book of revalation", "bojack", "pupu ni pepe", "kong", "senfhashm"],
+  year: [2025, 2017, 2022, 2009, 2000],
+  size: [100, "500kb", 56, "37kb", 325],
+  count: 5,
+};
+
+const divOutput = select("#buttonContainerr");
+
+//parent DOM obj
+const parent = select("#buttonContainer");
+
+const parentUL = select("#outputContainer");
+
+//create child
+
+//create object instances
+for (let x = 0; x < books.count; x++) {
+  bookshelf[x] = new Ebook(
+    books.title[x],
+    books.author[x],
+    books.year[x],
+    books.size[x]
+  );
+  //create button and set text
+  let buttonElement = document.createElement("button");
+  buttonElement.textContent = "book" + books.title[x];
+
+  //add functions to buttons
+
+  // create ne list
+  let listElement = document.createElement("li");
+  listElement.id = "list" + x;
+  parentUL.appendChild(listElement);
+
+  buttonElement.addEventListener("click", () => {
+    bookshelf[x].displayBook("list" + x);
+  });
+
+  // display buttons
+  parent.appendChild(buttonElement);
+}
