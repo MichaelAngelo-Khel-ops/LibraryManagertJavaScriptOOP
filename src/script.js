158a849a912b5dffcba94edfd373a2579cfcb4bb
MichaelAngelo-Khel-ops <michael1118salonga@gmail.com>
2025-09-21 11:06:34 +0800

    added new file and will do a input method for book

--- src/script.js
+++ src/script.js
@@ -17,65 +17,6 @@
 
 // add new comment on this branch
 
-class Book {
-  //private variables
-  #title;
-  #author;
-  #year;
-
-  constructor(title, author, year) {
-    this.#title = title;
-    this.#author = author;
-
-    if (typeof year != "number") {
-      throw new TypeError("Must be a number");
-    } else {
-      this.#year = year;
-    }
-  }
-
-  getTitle() {
-    return this.#title;
-  }
-  getAuthor() {
-    return this.#author;
-  }
-  getYear() {
-    return this.#year;
-  }
-
-  displayBook() {
-    return `${this.#title} by ${this.#author} : ${this.#year}`;
-  }
-}
-
-class Ebook extends Book {
-  #fileSize;
-
-  constructor(title, author, year, fileSize) {
-    super(title, author, year);
-
-    if (typeof fileSize == "string") {
-      this.#fileSize = parseInt(fileSize.replace("kb"));
-    } else {
-      this.#fileSize = fileSize;
-    }
-  }
-
-  displayBook(list) {
-    document.getElementById(
-      list
-    ).textContent = `${super.getTitle()} by ${super.getAuthor()} : ${super.getYear()} ${
-      this.#fileSize
-    }kb`;
-  }
-}
-
-// id selector
-function select(id) {
-  return document.querySelector(id);
-}
-
 // const ebook = new Ebook("Rascals", "Mike", 2025, 500);
 // add buttons
 const bookshelf = [];
